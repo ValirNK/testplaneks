81936bce2219e95b502dfb8b618c87671798ae30
ValirNK <top.trend2017@yandex.ru>
2021-06-27 16:25:46 +0300

    edit

--- csvmaster/settings.py
+++ csvmaster/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4*9p_ri#$nl1=6u-fc+whr4ac7y53szqw+cq8et7u$dj$1yvxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-	'185.16.31.83',
 	'https://testplaneks.herokuapp.com',
 	'localhost',
 	'127.0.0.1'

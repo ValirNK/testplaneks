561a1767e856205bf9bc3db30bdb8d8856b56c2d
ValirNK <top.trend2017@yandex.ru>
2021-07-02 23:57:13 +0300

    edit

--- csvmaster/views.py
+++ csvmaster/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from ..master.models import Musician
 
 
 def index(request):
     context = {'latest_question_list': 'dsf323'}
     return render(request, 'index.html', context)
+
+class Login(LoginRequiredMixin, ListView):
+    model = Musician
+    template_name = 'index.html'
